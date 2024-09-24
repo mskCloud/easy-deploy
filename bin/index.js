c1385aca38af04226479a4ab9654aab51497a4f6
mskCloud <1580231853@qq.com>
2024-09-24 18:57:49 +0800

    创建es版本

--- bin/index.js
+++ bin/index.js
@@ -1,44 +1,63 @@
 #! /usr/bin/env node
 
-const path = require("path")
-const { Command } = require("commander")
+import path from 'path'
+import url from 'node:url'
+import { Command } from 'commander'
 
-const packageJson = require("../package.json")
-const init = require("../lib/init.js")
-const deploy = require("../lib/deploy.js")
-const { failLog, successLog } = require("../tools/index.js")
+import init from '../lib/init.js'
+import deploy from '../lib/deploy.js'
+import { successLog, failLog, fetchFile } from '../tools/index.js'
 
-const program = new Command()
-const version = packageJson.version
+let version = '2.0.0'
 
-program.name("Easy Deploy Cli")
-program.description("Finally, you can get lazy about deploying projects")
-program.version(version, "-v, --version")
+const setVersion = async () => {
+    const packageJson = await fetchFile('/package.json', 'json')
+    version = packageJson.version
+    return version
+}
 
-program
-  .command("init")
-  .description("初始化部署相关配置")
-  .action(function () {
-    init()
-  })
+const createProgram = () => {
+    const program = new Command()
 
-program
-  .command("run <env>")
-  .description("根据选择的环境开始部署")
-  .option("-np --nopack", "跳过打包")
-  .option("-d --del", "删除dist文件夹")
-  .action(async function (env, option) {
-    try {
-      let config = require(path.join(path.resolve(), "/deploy/deploy.config.cjs"))
-      if (!config) {
-        config = require(path.join(path.resolve(), "/deploy/deploy.config.js"))
-      }
-      successLog("(1) 配置文件加载成功")
-      deploy(config, env, option)
-    } catch (error) {
-      failLog("(1) 配置文件加载失败", error)
-      throw error
-    }
-  })
+    program.name('Easy Deploy Cli')
+    program.description('Finally, you can get lazy about deploying projects')
+    program.version(version, '-v, --version')
 
-program.parse()
+    program
+        .command('init')
+        .description('初始化部署相关配置')
+        .action(function () {
+            init()
+        })
+
+    program
+        .command('test')
+        .description('初始化部署相关配置')
+        .action(function () {})
+
+    program
+        .command('run <env>')
+        .description('根据选择的环境开始部署')
+        .option('-np --nopack', '跳过打包')
+        .option('-d --del', '删除dist文件夹')
+        .action(async function (env, option) {
+            try {
+                const config = await fetchFile('/deploy/deploy.config.js')
+                successLog('(1) 配置文件加载成功')
+                console.log(config.default)
+                deploy(config.default, env, option)
+            } catch (error) {
+                failLog('(1) 配置文件加载失败', error)
+                throw error
+            }
+        })
+
+    program.parse()
+}
+
+async function createCli() {
+    await setVersion()
+    createProgram()
+}
+
+createCli()
--- tools/index.js
+++ tools/index.js
@@ -1,23 +1,33 @@
-// import chalk from "chalk"
-const chalk = require("chalk")
+import chalk from 'chalk'
+import path from 'node:path'
+import url from 'node:url'
+import { readFile } from 'fs/promises'
+
 // 获取当前日期并格式化为 YYYY-MM-DD-HH-MM-SS 格式
 const getCurrentDate = () => {
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = `0${date.getMonth() + 1}`.slice(-2)
-  const day = `0${date.getDate()}`.slice(-2)
-  const hour = date.getHours()
-  const minute = `0${date.getMinutes()}`.slice(-2)
-  const second = `0${date.getSeconds()}`.slice(-2)
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = `0${date.getMonth() + 1}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
+    const hour = date.getHours()
+    const minute = `0${date.getMinutes()}`.slice(-2)
+    const second = `0${date.getSeconds()}`.slice(-2)
+    return `${year}-${month}-${day}-${hour}-${minute}-${second}`
+}
 
-  return `${year}-${month}-${day}-${hour}-${minute}-${second}`
+const fetchFile = async (u, type) => {
+    const fileUrl = path.join(process.cwd(), u)
+    let res = null
+    if (type === 'json') {
+        res = await readFile(fileUrl)
+        res = JSON.parse(res)
+    } else {
+        res = await import(url.pathToFileURL(fileUrl))
+    }
+    return res
 }
 
 const successLog = (info, ...arg) => console.log(chalk.green(info, ...arg))
 const failLog = (info, ...arg) => console.log(chalk.red(info, ...arg))
 
-module.exports = {
-  getCurrentDate,
-  successLog,
-  failLog,
-}
+export { getCurrentDate, successLog, failLog, fetchFile }
